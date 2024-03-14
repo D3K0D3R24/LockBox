4eec3ab5bd88edf9ee554003c1b856a3ce853360
felipe <104036699+felipeduarte2@users.noreply.github.com>
2024-03-13 20:23:00 -0600

    Codigo corregido
    ya se puede eliminar las contraseñas

--- PasswordManager/app/src/main/java/com/example/passwordmanager/BdPassword/MyDataClassAdapter.kt
+++ PasswordManager/app/src/main/java/com/example/passwordmanager/BdPassword/MyDataClassAdapter.kt
@@ -11,20 +11,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.example.passwordmanager.R
 
-//
-//interface ItemTouchHelperAdapter {
-//    fun onItemDismiss(position: Int)
-//}
-class MyDataClassAdapter(val datalist: List<Contrasenas>) :RecyclerView.Adapter<MyDataClassAdapter.viewHolderClass>(){
+class MyDataClassAdapter(private val datalist: MutableList<Contrasenas>) : RecyclerView.Adapter<MyDataClassAdapter.viewHolderClass>() {
 
-    class viewHolderClass(itemView: View): RecyclerView.ViewHolder(itemView) {
-        lateinit var imageView : ImageView
-        lateinit var titleTextView : TextView
-        lateinit var passwordTextView : TextView
+    class viewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var imageView: ImageView
+        lateinit var titleTextView: TextView
+        lateinit var passwordTextView: TextView
+
         init {
             imageView = itemView.findViewById(R.id.imageView)
             titleTextView = itemView.findViewById(R.id.titleTextView)
@@ -33,7 +31,7 @@ class MyDataClassAdapter(val datalist: List<Contrasenas>) :RecyclerView.Adapter<
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolderClass {
-        val vista = LayoutInflater.from(parent.context).inflate(R.layout.diseno_recicleview,parent,false)
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.diseno_recicleview, parent, false)
         return viewHolderClass(vista)
     }
 
@@ -42,23 +40,25 @@ class MyDataClassAdapter(val datalist: List<Contrasenas>) :RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: viewHolderClass, position: Int) {
-        holder.imageView.setImageBitmap(BitmapFactory.decodeByteArray(datalist[position].image, 0, datalist[position].image.size))
-        holder.titleTextView.text = datalist[position].title
-        holder.passwordTextView.text = datalist[position].password
+        val contrasena = datalist[position]
+        holder.imageView.setImageBitmap(BitmapFactory.decodeByteArray(contrasena.image, 0, contrasena.image.size))
+        holder.titleTextView.text = contrasena.title
+        holder.passwordTextView.text = contrasena.password
 
         val layoutParams = holder.itemView.layoutParams as ViewGroup.MarginLayoutParams
         layoutParams.setMargins(16, 16, 16, 16)
         holder.itemView.layoutParams = layoutParams
 
         holder.itemView.setOnClickListener {
-            //al darle click a un item
+            // al darle clic a un elemento
+            val context = holder.itemView.context
+            val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("contrasena_title", contrasena.password)
+            clipboardManager.setPrimaryClip(clip)
+            //val myDataClassDao = ContraseñasCrud(MyDatabaseHelper(holder.itemView.context))
+            val message = "Contraseña copiado al portapapeles: ${contrasena.password}"
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
         }
     }
-
-
-//    fun onItemDismiss(position: Int) {
-//        //datalist.removeAt(position)
-//        notifyItemRemoved(position)
-//    }
-
 }
+
